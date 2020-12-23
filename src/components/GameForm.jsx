d9c1a4e6d31ac1933807457faced4b27d78026b2
Niccolò Toccane <nico.toccane@gmail.com>
2020-12-23 14:24:34 +0100

    implement create new game feature

--- src/components/GameForm.jsx
+++ src/components/GameForm.jsx
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import '../styles/compileds/GameForm.css';
+import { createGame } from '../helpers/api';
+
+const APP_URL = 'http://localhost:3000';
 
 class GameForm extends Component {
 	static defaultProps = {
@@ -11,7 +14,11 @@ class GameForm extends Component {
 	constructor (props) {
 		super(props);
 		const { defaultAttempts } = this.props;
-		this.state = { form: { word: '', attempts: defaultAttempts }, error: null };
+		this.state = {
+			form     : { word: '', attempts: defaultAttempts },
+			error    : null,
+			gameLink : null
+		};
 	}
 
 	handleChange = e => {
@@ -19,6 +26,7 @@ class GameForm extends Component {
 		let { name, value } = e.target;
 		const { maxAttempts, minAttempts } = this.props;
 		value = name === 'attempts' ? +value : value;
+		value = name === 'word' ? value.toLowerCase() : value;
 		if (name === 'attempts' && (value < minAttempts || value > maxAttempts)) {
 			value = value > maxAttempts ? maxAttempts : minAttempts;
 		}
@@ -28,7 +36,7 @@ class GameForm extends Component {
 		}));
 	};
 
-	handleSubmit = e => {
+	handleSubmit = async e => {
 		e.preventDefault();
 		const { defaultAttempts } = this.props;
 		const { word, attempts } = this.state.form;
@@ -36,6 +44,8 @@ class GameForm extends Component {
 			let r = new RegExp(/\d/);
 			if (!r.test(word)) {
 				// valid form
+				let game = await createGame({ word, attempts });
+				this.setState({ gameLink: `${APP_URL}/${game._id}` });
 			} else {
 				this.setState(s => ({
 					...s,
@@ -55,31 +65,41 @@ class GameForm extends Component {
 	};
 
 	render () {
-		const { form, error } = this.state;
+		const { form, error, gameLink } = this.state;
 		return (
-			<form className="GameForm" onSubmit={this.handleSubmit}>
-				<h1>Create new hangman</h1>
-				{!!error && <p className="GameForm-error-alert">{error}</p>}
-				<input
-					className="GameForm-input"
-					type="text"
-					placeholder="Word"
-					name="word"
-					value={form.word}
-					onChange={this.handleChange}
-				/>
-				<input
-					className="GameForm-input"
-					type="number"
-					placeholder="Attempts"
-					name="attempts"
-					value={form.attempts}
-					onChange={this.handleChange}
-				/>
-				<button className="GameForm-button" type="submit">
-					Create Game!
-				</button>
-			</form>
+			<div>
+				{gameLink && (
+					<div>
+						<h1>{gameLink}</h1>
+						<button type="button">COPY</button>
+					</div>
+				)}
+				{!gameLink && (
+					<form className="GameForm" onSubmit={this.handleSubmit}>
+						<h1>Create new hangman</h1>
+						{!!error && <p className="GameForm-error-alert">{error}</p>}
+						<input
+							className="GameForm-input"
+							type="text"
+							placeholder="Word"
+							name="word"
+							value={form.word}
+							onChange={this.handleChange}
+						/>
+						<input
+							className="GameForm-input"
+							type="number"
+							placeholder="Attempts"
+							name="attempts"
+							value={form.attempts}
+							onChange={this.handleChange}
+						/>
+						<button className="GameForm-button" type="submit">
+							Create Game!
+						</button>
+					</form>
+				)}
+			</div>
 		);
 	}
 }
