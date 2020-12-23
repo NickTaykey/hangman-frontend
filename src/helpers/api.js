4ed8244b8b6c5fa883c73c0115e092b5f0345385
Niccolò Toccane <nico.toccane@gmail.com>
2020-12-23 10:55:01 +0100

    implement create new game feature

--- src/helpers/api.js
+++ src/helpers/api.js
@@ -1,12 +1,23 @@
+import axios from 'axios';
+
 function getGameInfo (id) {
 	return new Promise(async (resolve, reject) => {
-		let res = await fetch(`/${id}`);
-		let { error, ...game } = await res.json();
+		let { error, ...res } = await axios.get(`/${id}`);
 		if (!error) {
-			return resolve(game);
+			return resolve(res.data);
 		}
 		return reject(error);
 	});
 }
 
-export { getGameInfo };
+function createGame (data) {
+	return new Promise(async (resolve, reject) => {
+		let { error, ...res } = await axios.post('/', data);
+		if (!error) {
+			return resolve(res.data);
+		}
+		return reject(error);
+	});
+}
+
+export { getGameInfo, createGame };
